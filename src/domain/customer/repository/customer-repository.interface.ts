e96acfd015ee395bf8964d62d0842bddb7f340ee
venilson2 <venilson2009@hotmail.com>
2024-11-05 20:40:14 -0300

    first commit

---
+++ src/domain/customer/repository/customer-repository.interface.ts
@@ -0,0 +1,4 @@
+import RepositoryInterface from "../../@sahred/repository/repository-interface";
+import Customer from "../entity/customer";
+
+export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> {}
