ec57fd2f4741ad98982890ea3df8a88b02f22d79
venilson2 <venilson2009@hotmail.com>
2024-11-06 15:06:48 -0300

    refactor: change customer to product

--- src/domain/customer/repository/customer-repository.interface.ts
+++
@@ -1,4 +0,0 @@
-import RepositoryInterface from "../../@sahred/repository/repository-interface";
-import Customer from "../entity/customer";
-
-export default interface CustomerRepositoryInterface extends RepositoryInterface<Customer> {}
---
+++ src/domain/product/entity/product.ts
@@ -0,0 +1,17 @@
+export default class Product {
+  private _id: string = "";
+  private _name: string = "";
+
+  constructor(id: string, name: string) {
+    this._id = id;
+    this._name = name;
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+}
